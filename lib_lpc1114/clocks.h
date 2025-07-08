aa668ce2ef5b67f3534504fdc17470ac9e5c28be
Glauber da Silva Moura <silvamouraglauber@outlook.com>
2025-07-08 09:52:47 -0300

    update: add uart clock support

--- lib_lpc1114/clocks.h
+++ lib_lpc1114/clocks.h
@@ -13,9 +13,20 @@ void clock_i2c(){
 	// habilitar o clock para o bloco I2C (UM10398, 3.5.14)
 	LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 5);
 }
+void clock_uart(){
+	// habilitar o clock para o bloco UART (UM10398, 3.5.14)
+	LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 12);
+	// estipular o clock do módulo UART (divisor de 0x9B) para gerar BR de 19.2K (UM10398, 3.5.16)
+	LPC_SYSCON->UARTCLKDIV |= 0x9B;
+}
+void clock_adc(){
+	// habilitar o clock para o bloco ADC (UM10398, 3.5.14)
+	LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 13);
+}
 void clocks_config(){
 	clock_gpio();
 	clock_iocon();
 	clock_i2c();
+	clock_uart();
 }
 
