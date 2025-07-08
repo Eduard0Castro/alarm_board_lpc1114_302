ec19c73ef74489225144dfa2e148ccea14b7d6fb
Glauber da Silva Moura <silvamouraglauber@outlook.com>
2025-07-08 10:20:31 -0300

    refactor: test adc using leds and lcd

--- lib_lpc1114/adc_main.c
+++ lib_lpc1114/adc_main.c
@@ -1,6 +1,7 @@
 #include "clocks.h"
 #include "adc.h"
 #include "LCD.h"
+#include "LED.h"
 
 int main(){
     clocks_config();
@@ -8,12 +9,31 @@ int main(){
     ADC_pin_config(0);
     LCD_pin_config();
     LCD_iniciar();
+    configureLED();
+    apagaLEDS();
 
     int adc_value;
     while(1){
         LCD_cmd(0x80);
         adc_value = ADC_read();
         LCD_Int(adc_value);
+        if (adc_value < 256) {
+            toggleLED(1);
+            for(int i = 0; i < 0x3FFF; i++);
+        }
+        else if (adc_value < 512) {
+            toggleLED(2);
+            for(int i = 0; i < 0x3FFF; i++);
+        }
+        else if (adc_value < 767) {
+            toggleLED(3);
+            for(int i = 0; i < 0x3FFF; i++);
+        }
+        else {
+            toggleLED(4);
+            for(int i = 0; i < 0x3FFF; i++);
+        }
+        apagaLEDS();
     }
     return 0;
 }
